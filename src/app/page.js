262c8a1e45f0929fe61f6e250d54a22f43f4cfab
yelmouss <yelmouss.devt@gmail.com>
2025-09-22 21:26:44 +0100

    refactor: Remove unused components and clean up globals.css

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,9 @@
 import Generator from "@/components/Generator";
-import { Boxes } from "@/components/ui/shadcn-io/background-boxes";
 import { Container } from "@mui/material";
-import { FlickeringGrid } from "@/components/ui/shadcn-io/flickering-grid";
 export default function Home() {
   return (
     <>
-      <Container>
-        <FlickeringGrid
-          className="absolute inset-0"
-          squareSize={4}
-          gridGap={6}
-          flickerChance={0.3}
-          color="rgb(100, 100, 100)"
-          maxOpacity={0.2}
-          height={"100%"}
-        />
+      <Container>      
         <Generator />
       </Container>
     </>
